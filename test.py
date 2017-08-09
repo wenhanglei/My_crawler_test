ed422b3e79e2a2e3a28716eef93f153752aa3a0a
wenhanglei <3188744608@qq.com>
2017-08-09 16:50:35 +0800

    add script to test pymysql

--- test.py
+++ test.py
@@ -1,10 +1,8 @@
-import csv
-
-csvFile = open("test.csv", 'w')
-try:
-    writer = csv.writer(csvFile)
-    writer.writerow(('number', 'number plus 2', 'number times 2'))
-    for i in range(10):
-        writer.writerow((i, i+2, i*2))
-finally:
-    csvFile.close()
+import pymysql
+conn = pymysql.connect(host='localhost', user='root', passwd='712342', db='mysql')
+cur = conn.cursor()
+cur.execute("USE scraping")
+cur.execute("select * from pages")
+print(cur.fetchone())
+cur.close()
+conn.close()
