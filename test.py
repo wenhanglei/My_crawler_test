294a0482a9ed30e1ac6fb80c5ed70085b5ab76bd
wenhanglei <3188744608@qq.com>
2017-08-17 16:29:05 +0800

    add download html function

--- test.py
+++ test.py
@@ -1,8 +1,31 @@
-import pymysql
-conn = pymysql.connect(host='localhost', user='root', passwd='****', db='mysql')
-cur = conn.cursor()
-cur.execute("USE scraping")
-cur.execute("select * from pages")
-print(cur.fetchone())
-cur.close()
-conn.close()
+import requests
+#def download(url):
+#    return requests.get(url).content
+#
+# ###################################################
+# def download(url):
+#     print('downloading: ' + url)
+#     try:
+#         html = requests.get(url).content
+#     except requests.HTTPError as e:
+#         print('something is wrong!')
+#         html = None
+#     return html
+###################################################
+def download(url, num_retrieve=2):
+    print('downloading: ' + url)
+    try:
+        html = requests.get(url).content
+    except requests.HTTPError as e:
+        print('something is wrong!')
+        html = None
+        if num_retrieve > 0:
+            code = e.respoonse.status_code
+            if code != None and 500 <= code < 600:
+                return download(url, num_retrieve-1)
+    return html
+
+url = 'http://www.ilxdh.com'
+html = download(url)
+with open('text.html', 'wb') as file:
+    file.write(html)
