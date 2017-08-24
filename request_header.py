6f1981896be24fc50943f6275831644e401a77cc
wenhanglei <3188744608@qq.com>
2017-08-24 15:48:48 +0800

    initialize the dir004

--- request_header.py
+++
@@ -1,5 +0,0 @@
-header = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36',
-           'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-           'Accept-Encoding': 'gzip, deflate',
-           'Connection': 'keep-alive',
-           'Accept-Language': 'zh-CN,zh;q=0.8,en-US;q=0.6,en;q=0.4'}
