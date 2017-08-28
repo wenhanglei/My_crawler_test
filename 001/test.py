af01d3081f7f7114f4960552ac8f02c1e19204d4
wenhanglei <3188744608@qq.com>
2017-08-28 22:47:14 +0800

    change dir_001

--- 001/test.py
+++ 001/test.py
@@ -1,23 +1,84 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import requests
-import base64
-import io
-import time
+from bs4 import BeautifulSoup
+import re
 
-def download(id):
-    return 'http://sited.ka94.com' + '/plugin.sited.php?id=' + str(id) + '&t=' + str(int(time.time()) + 180)
+#设定访问的入口网址:使用孔夫子旧书网做参考
+entry = 'http://www.kongfz.com/'
+#请求头文件设定
+file = 'header.txt'
+#用于保存链接的set
+links = set()
 
-def get_url(text):
-    data = base64.b64decode(bytes(text.split('?')[1],'utf-8'))
-    url = data.decode('utf-8')
-    return url
+#通过header.txt载入用于设置request的header
+def load_header(filename):
+    header = {}
+    with open(filename) as file:
+        for line in file:
+            key,value = line.rstrip().split(':', maxsplit=1)
+            header[key] = value
+    return header
 
-# r = requests.get(download(65))
-# r.encoding = 'utf-8'
-# with open('file.xml', 'w', encoding='utf-8') as file:
-#     file.write(r.text)
+###################################################################
+#测试是否不需要修改ug头而且不需要cookies就可以访问
+# resp = requests.get(entry)
+# resp.raise_for_status()
+#结论：不能访问
+###################################################################
 
-def save_as_xml(url):
-    resp = requests.get(url)
-    resp.encoding = 'utf-8'
-    with open('siteD.xml', 'w', encoding='utf-8') as file:
-        file.write(resp.text)
+###################################################################
+#测试修改ug头后是否可以访问
+# #获取ug头
+ug = load_header('header.txt')
+#设置ug并访问网站
+resp = requests.get(entry, headers=ug)
+# resp.raise_for_status()
+#结论：网站通过ug控制了用户的访问
+###################################################################
+
+###################################################################
+#使用BeautifulSoup解析网站获取所有的连接对象
+resp.encoding = 'utf-8'
+bsobj = BeautifulSoup(resp.text, 'html.parser')
+#查找所有的a标签
+tags = bsobj.find_all('a')
+print(len(tags))
+#遍历a标签
+for tag in tags:
+    if 'href' in tag.attrs:
+        #使用regex剔除所有的外链
+        p = re.compile(r'http.*kongfz.*')
+        m = p.match(tag['href'])
+        #如果匹配
+        if m:
+            #向links添加连接
+            links.add(m.group())
+            print(m.group())
+        else:
+            continue
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
