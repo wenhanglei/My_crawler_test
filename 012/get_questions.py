366698e346b9c7cbdf96dccfacf012d49369b579
wenhanglei <3188744608@qq.com>
2017-09-06 17:04:18 +0800

    complete dir_012

--- 012/get_questions.py
+++ 012/get_questions.py
@@ -3,13 +3,17 @@
 
 import util
 import requests
+import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoSuchElementException
 from bs4 import BeautifulSoup
 
 #请求url
-req_url = 'https://www.zhihu.com/people/yu-tian-50-3/following/questions?page=59'
+req_url = 'https://www.zhihu.com/people/yu-tian-50-3/following/questions'
 
 def get_questions(result):
     try:
@@ -25,16 +29,31 @@ def get_questions(result):
             result.append(ques.string)
         return result
     else:
-        #构造bsobj
+        time.sleep(0.5)
+        # 构造bsobj
         bsobj = BeautifulSoup(driver.page_source, 'html.parser')
-        #获取包含item的容器
-        div = bsobj.find('div', id="Profile-following")
+        div = bsobj.find('div', id='Profile-following')
         ques_div = div.find_all('div', class_="QuestionItem-title")
         for ques in ques_div:
             result.append(ques.string)
         #跳转到下一页
         btn.send_keys(Keys.RETURN)
         return get_questions(result)
+
+def get_ques(result, wait):
+    try:
+        btn = driver.find_element_by_class_name('PaginationButton-next')
+    except NoSuchElementException:
+        divs = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'QuestionItem-title')))
+        for div in divs:
+            result.append(div.find_element_by_tag_name('a').text)
+        return result
+    else:
+        divs = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, 'QuestionItem-title')))
+        for div in divs:
+            result.append(div.find_element_by_tag_name('a').text)
+        btn.send_keys(Keys.RETURN)
+        return get_ques(result, wait)
 
 #加载cookies
 # cookies = util.get_cookie_dic_from_file('cookie.txt')
@@ -70,29 +89,21 @@ def get_questions(result):
 #     # for que in ques_div:
 #     #     print(que.string)
 
+# driver = webdriver.Chrome()
+# driver.get(req_url)
+# #获取结果集
+# result = []
+# get_questions(result)
+# #保存获取的结果
+# util.save_obj(result, 'questions.pkl')
+# driver.close()
+
 driver = webdriver.Chrome()
 driver.get(req_url)
-#获取结果集
 result = []
-btn = driver.find_element_by_class_name('PaginationButton-next')
-#构造bsobj
-bsobj = BeautifulSoup(driver.page_source, 'html.parser')
-#获取包含item的容器
-div = bsobj.find('div', id="Profile-following")
-ques_div = div.find_all('div', class_="QuestionItem-title")
-for ques in ques_div:
-    result.append(ques.string)
-#跳转到下一页
-btn.send_keys(Keys.RETURN)
-#构造bsobj
-bsobj = BeautifulSoup(driver.page_source, 'html.parser')
-#获取包含item的容器
-div = bsobj.find('div', id="Profile-following")
-ques_div = div.find_all('div', class_="QuestionItem-title")
-for ques in ques_div:
-    result.append(ques.string)
-for re in result:
-    print(re)
+wait = WebDriverWait(driver, 10)
+get_ques(result, wait)
+print(len(result))
+util.save_obj(result, 'questions.pkl')
+time.sleep(1)
 driver.close()
-
-
--- 012/test_result.py
+++ 012/test_result.py
@@ -1,2 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+import util
+
+result = util.load_obj('quesions.pkl')
+print(len(result))
