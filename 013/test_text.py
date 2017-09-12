e59e77d963b80da51589e9c5e32bc17aa0b7c7b2
wenhanglei <3188744608@qq.com>
2017-09-12 10:23:10 +0800

    some little changes

--- 013/test_text.py
+++ 013/test_text.py
@@ -8,9 +8,4 @@ text_div = util.load_obj('text_div')
 # print(text_div)
 #构造beautifulsoup对象
 bsobj = BeautifulSoup(text_div, 'html.parser')
-ps = bsobj.find_all('p')
-string = ''
-for p in ps:
-    string += p.get_text().strip()
-for s in string.split('.'):
-    print(s)
+print(bsobj.get_text())
