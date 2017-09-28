5f6310cb113151ea5d72b94849d4afb55a5dcabf
wenhanglei <3188744608@qq.com>
2017-09-28 11:10:51 +0800

    complete dir_014

--- 014/test_data.py
+++ 014/test_data.py
@@ -4,6 +4,7 @@
 import pymysql
 import sqlutil
 import re
+import time
 
 def get_time_data(s):
     regex = re.compile(r'(\d*) 分钟')
@@ -14,7 +15,7 @@ def get_time_data(s):
 #连接数据库获得连接对象
 connection = pymysql.connect(host='localhost',
                              user='root',
-                             password='712342',
+                             password='*****',
                              db='test',
                              charset='utf8')
 # #获取光标对象
@@ -22,9 +23,14 @@ cursor = connection.cursor()
 sql = 'select * from study;'
 cursor.execute(sql)
 sum = 0
+start = time.time()
 for r in cursor.fetchall():
     sum += get_time_data(r[1])
-print(str(sum) + ' --- ' + str(sum/10))
+end = time.time()
+elp_time = '%.2f' % (end-start)
+study_time = str(sum/60)
+print('本次统计耗时 %s 秒' % elp_time)
+print('你总共学习了 %s 小时！' % sum)
 
 
 
--- 014/test_time.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import time
-
-start = time.time()
-time.sleep(5)
-end = time.time()
